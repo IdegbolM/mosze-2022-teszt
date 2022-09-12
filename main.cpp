658f52f5d4f0ae11336d092400e10fb8979f11f3
IdegbolM <71631249+IdegbolM@users.noreply.github.com>
2022-09-12 12:52:52 +0200

    Hibák dokumentálása

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //int* b, N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa'  //dupla aposztróf és pontosvessző a sor végére, + egy sortörés
+    for (int i = 0;) // i=1, kihagyva a határ és a növekvés: i<=100; i++
     {
-        b[i] = i * 2;
+        b[i] = i * 2;   //b[i-1]
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)     //felesleges ciklus, az elsőbe beletenni a törzsöt
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"   //a konkrét érték kiíratása (b[i-1]) sortöréssel és pontosvessző a végére
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; //kezdőérték (0)
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i]   //pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
