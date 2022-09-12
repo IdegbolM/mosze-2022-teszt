869f4d7fb55a77b71cbc5ae68904baf5ff5c04b6
IdegbolM <71631249+IdegbolM@users.noreply.github.com>
2022-09-12 13:03:31 +0200

    Hibák kijavítása
    A hibák javítását megjeleníti

--- main.cpp
+++ main.cpp
@@ -4,21 +4,18 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //int* b, N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa'  //dupla aposztróf és pontosvessző a sor végére, + egy sortörés
-    for (int i = 0;) // i=1, kihagyva a határ és a növekvés: i<=100; i++
+    int* b = new int[N_ELEMENTS]; //int* b, N_ELEMENTS
+    std::cout << "1-100 ertekek duplazasa\n";  //dupla aposztróf és pontosvessző a sor végére, + egy sortörés
+    for (int i = 1; i<=N_ELEMENTS; i++) // i=1, kihagyva a határ és a növekvés: i<=100; i++
     {
-        b[i] = i * 2;   //b[i-1]
+        b[i-1] = i * 2;   //b[i-1]
+        std::cout << "Ertek:" << b[i - 1] << std::endl; //felesleges ciklus, az elsőbe beletenni a törzsöt, a konkrét érték kiíratása (b[i-1]) sortöréssel és pontosvessző a végére
     }
-    for (int i = 0; i; i++)     //felesleges ciklus, az elsőbe beletenni a törzsöt
-    {
-        std::cout << "Ertek:"   //a konkrét érték kiíratása (b[i-1]) sortöréssel és pontosvessző a végére
-    }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //kezdőérték (0)
+    int atlag = 0; //kezdőérték (0)
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]   //pontosvessző
+        atlag += b[i];   //pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
